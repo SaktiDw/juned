d571eae5bfd585559b5a5f0193f8e2966e75b3ee
SaktiDw <sakti596@gmail.com>
2023-02-26 18:39:35 +0800

    configure component export

---
+++ components/index.jsx
@@ -0,0 +1,11 @@
+export { default as MainLayout } from "./Layouts/MainLayout";
+
+export { default as FormUsers } from "./Forms/FormUsers";
+export { default as Input } from "./Forms/Input";
+
+export { default as Table } from "./Tables/Table";
+export { default as Action } from "./Tables/Action";
+
+export { default as Nav } from "./Navigations/Nav";
+export { default as Sidebar } from "./Navigations/Sidebar";
+export { default as SidebarItem } from "./Navigations/SidebarItem";
