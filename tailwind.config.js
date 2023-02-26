0f58ac9357f0d5bf8c62d4fce7db667c5bbe08f9
SaktiDw <sakti596@gmail.com>
2023-02-26 18:37:59 +0800

    install package

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ ],
+  theme: {
+    // colors: {
+    //   transparent: 'transparent',
+    //   current: 'currentColor',
+    //   primary: '#2892d7',
+    //   secondary: '#6daedb'
+    // },
+    extend: {},
+  },
+  plugins: [],
+}
