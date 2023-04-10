b10bcbfbb375e3995276a89cbae06cffede9c791
SaktiDw <sapeiidw@gmail.com>
2023-04-10 14:27:09 +0800

    page anggota profesi

---
+++ components/Forms/FormCreateAnggotaProfesi.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import { Formik, Form } from "formik";
+import {
+  Button,
+  Input,
+  KategoriKegiatanSelection,
+  MultipleUploadFile,
+  NestedList,
+  PerguruanTinggiSelection,
+  Select,
+  StackedTab,
+  Textarea,
+} from "..";
+import * as yup from "yup";
+import { createUser, fetchListInpassing } from "@/helper/api/api";
+import { useQuery } from "@tanstack/react-query";
+
+const schema = yup.object().shape({
+  kategori_kegiatan: yup.string().required("kategori kegiatan wajib di isi."),
+  nama_organisasi: yup.string().required("tingkat penghargaan wajib di isi."),
+  peran: yup.string().required("jenis penghargaan wajib di isi."),
+  mulai_keanggotaan: yup.string().required("nama penghargaan wajib di isi."),
+  selesai_keanggotaan: yup
+    .string()
+    .required("selesai_keanggotaan wajib di isi."),
+  instansi_profesi: yup
+    .string()
+    .required("bidang ahli pembimbing wajib di isi."),
+});
+
+const FormCreateAnggotaProfesi = () => {
+  return (
+    <>
+      <Formik
+        enableReinitialize
+        initialValues={{
+          kategori_kegiatan: "",
+          nama_organisasi: "",
+          peran: "",
+          mulai_keanggotaan: "",
+          selesai_keanggotaan: "",
+          instansi_profesi: "",
+        }}
+        validationSchema={schema}
+        onSubmit={(values, { setErrors, setStatus }) =>
+          createUser(values)
+            .then((res) => setStatus(res.data))
+            .catch(
+              (err) => err
+              // err.status === "422" && setErrors(err.response.data.message)
+            )
+        }
+      >
+        {({ isSubmitting, errors, touched, status, isValid }) => (
+          <Form className="flex flex-col gap-4">
+            <KategoriKegiatanSelection
+              errors={errors.kategori_kegiatan}
+              touched={touched.kategori_kegiatan}
+            />
+            <Input
+              label="nama organisasi"
+              name="nama_organisasi"
+              type="text"
+              errors={errors.nama_organisasi}
+              touched={touched.nama_organisasi}
+            />
+            <Input
+              label="peran"
+              name="peran"
+              type="text"
+              errors={errors.peran}
+              touched={touched.peran}
+            />
+            <Input
+              label="mulai keanggotaan"
+              name="mulai_keanggotaan"
+              type="date"
+              errors={errors.mulai_keanggotaan}
+              touched={touched.mulai_keanggotaan}
+            />
+            <Input
+              label="selesai keanggotaan"
+              name="selesai_keanggotaan"
+              type="date"
+              errors={errors.selesai_keanggotaan}
+              touched={touched.selesai_keanggotaan}
+            />
+            <MultipleUploadFile />
+            <Button
+              disabled={!isValid}
+              type={"submit"}
+              text={isSubmitting ? "Loading..." : "Ajukan perubahan"}
+            />
+          </Form>
+        )}
+      </Formik>
+    </>
+  );
+};
+
+export default FormCreateAnggotaProfesi;
---
+++ pages/anggota-profesi/index.jsx
@@ -0,0 +1,34 @@
+import { Action, MainLayout, Nav, Table } from "@/components";
+import React from "react";
+
+const AnggotaProfesi = () => {
+  return (
+    <MainLayout>
+      <div className="flex flex-col gap-4 w-full">
+        <Nav title={"Anggota Profesi"} />
+        <h1 className="text-md uppercase font-bold drop-shadow-lg shadow-white">
+          Anggota Profesi
+        </h1>
+        <Table
+          createLink={"/anggota-profesi/create"}
+          columns={[
+            { key: "id", title: "No." },
+            { key: "nama_organisasi", title: "Anggota Profesi" },
+            { key: "peran_kedudukan", title: "Peran/Kedudukan" },
+            { key: "mulai_keanggotaan", title: "Mulai Keanggotaan" },
+            { key: "selesai_keanggotaan", title: "Selesai Keanggotaan" },
+            { key: "instansi_profesi", title: "Instansi" },
+            { key: "rubrik_bkd", title: "Rubrik BKD" },
+            {
+              key: "id",
+              title: "Action",
+              render: (val) => <Action param={val} />,
+            },
+          ]}
+        />
+      </div>
+    </MainLayout>
+  );
+};
+
+export default AnggotaProfesi;
