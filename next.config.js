ec2492c3b4b4fc9c333a83339ecb0c9de9630719
SaktiDw <sapeiidw@gmail.com>
2023-04-16 17:46:31 +0800

    pasang proxy biar gk kena cors

--- next.config.js
+++ next.config.js
@@ -3,4 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/sister/:path*',
+        destination: 'http://sister.itk.ac.id/ws.php/1.0/:path*',
+      }
+    ]
+  },
+  ...nextConfig,
+}
