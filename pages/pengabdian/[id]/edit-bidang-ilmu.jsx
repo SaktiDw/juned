06d4478138a3dddc51761acf00b31d9c4bd94f1e
SaktiDw <sapeiidw@gmail.com>
2023-04-19 23:47:46 +0800

    fix pengabdian

--- pages/pengabdian/[id]/edit-bidang-ilmu.jsx
+++ pages/pengabdian/[id]/edit-bidang-ilmu.jsx
@@ -1,27 +1,27 @@
 import {
   KelompokBidangSelection,
-  FormCreatePenelitian,
+  FormCreatePenngabdian,
   MainLayout,
   Nav,
   Button,
 } from "@/components";
-import { fetchDetailPenelitian } from "@/helper/api/apiSister";
+import { fetchDetailPengabdian } from "@/helper/api/apiSister";
 import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 
-const PenelitianEditBidangIlmu = () => {
+const PenngabdianEditBidangIlmu = () => {
   const router = useRouter();
   const { id } = router.query;
   useEffect(() => {}, [router.isReady]);
 
   const {
-    data: penelitian,
+    data: pengabdian,
     isError,
     isLoading,
   } = useQuery({
-    queryKey: ["penelitian", id],
-    queryFn: async () => await fetchDetailPenelitian(id),
+    queryKey: ["pengabdian", id],
+    queryFn: async () => await fetchDetailPengabdian(id),
     networkMode: "offlineFirst",
   });
   {
@@ -33,12 +33,12 @@ const PenelitianEditBidangIlmu = () => {
   return (
     <MainLayout>
       <div className="flex flex-col gap-8">
-        <Nav title={"Edit Penelitian"} />
+        <Nav title={"Edit Penngabdian"} />
         <div className="flex flex-col gap-4 dark:text-white w-full h-full">
-          <h1 className="text-md font-bold uppercase">Form Edit Penelitian</h1>
+          <h1 className="text-md font-bold uppercase">Form Edit Penngabdian</h1>
           <div className="grid grid-flow-row gap-4 bg-white dark:bg-slate-800 px-4 py-8 rounded-xl ">
             <h1 className="text-lg font-bold text-slate-600 dark:text-slate-500">
-              {penelitian?.judul}
+              {pengabdian?.judul}
             </h1>
             <div className="flex gap-8">
               <div className="flex flex-col gap-2">
@@ -57,4 +57,4 @@ const PenelitianEditBidangIlmu = () => {
   );
 };
 
-export default PenelitianEditBidangIlmu;
+export default PenngabdianEditBidangIlmu;
