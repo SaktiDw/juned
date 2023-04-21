395dc234bb65cae36d0493d9883c1beca9cdef29
SaktiDw <sapeiidw@gmail.com>
2023-04-21 14:14:39 +0800

    penunjang lain add document

--- pages/penunjang-lain/index.jsx
+++ pages/penunjang-lain/index.jsx
@@ -1,13 +1,21 @@
-import { Action, MainLayout, Nav, Table } from "@/components";
+import {
+  Action,
+  MainLayout,
+  ModalTambahDokumen,
+  Nav,
+  Table,
+} from "@/components";
 import {
   fetchListAnggotaProfesi,
   fetchListPenunjangLain,
 } from "@/helper/api/apiSister";
 import { id } from "@/helper/constant";
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
+import React, { useState } from "react";
 
 const AnggotaProfesi = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [selected, setSelected] = useState();
   const {
     data: penunjang_lain,
     isLoading,
@@ -17,7 +25,15 @@ const AnggotaProfesi = () => {
     queryFn: () => fetchListPenunjangLain(id),
   });
   return (
-    <MainLayout>
+    <MainLayout
+      modal={
+        <ModalTambahDokumen
+          title={selected}
+          showModal={showModal}
+          setShowModal={() => setShowModal(!showModal)}
+        />
+      }
+    >
       <div className="flex flex-col gap-4 w-full">
         <Nav title={"Penunjang Lain"} />
         <h1 className="text-md uppercase font-bold drop-shadow-lg shadow-white">
@@ -45,7 +61,14 @@ const AnggotaProfesi = () => {
               key: "id",
               title: "Action",
               render: (val) => (
-                <Action param={val} baseUrl={"/penunjang-lain"} />
+                <Action
+                  param={val}
+                  baseUrl={"/penunjang-lain"}
+                  addDocumentFn={() => {
+                    setShowModal(!showModal);
+                    setSelected(val?.nama);
+                  }}
+                />
               ),
             },
           ]}
