ce9b913fd62f278538789b7296cbf9765861e59f
SaktiDw <sapeiidw@gmail.com>
2023-04-21 13:23:53 +0800

    redirect to login page if not authenticated

--- helper/api/axios.js
+++ helper/api/axios.js
@@ -16,6 +16,7 @@ if (typeof window !== 'undefined') {
     const item = localStorage.getItem('key')
   }
 const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null
+
 export const apiSister = axios.create({
     baseURL: "/sister/",
     headers: {
@@ -24,3 +25,19 @@ export const apiSister = axios.create({
         'Content-Type': 'application/json',
     },  
 })
+
+apiSister.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      const config = error.config;
+      if (error.response && error.response.status === 401 && !config._retry) {
+        config._retry = true;
+  
+        window.location.href = "/auth/login";
+        return Promise.reject(error);
+      }
+      return Promise.reject(error);
+    }
+  );
