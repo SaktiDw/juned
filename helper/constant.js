b4b26f31012c811aad21b655520da683b296ed76
SaktiDw <sapeiidw@gmail.com>
2023-04-21 15:36:17 +0800

    add sort fun

--- helper/constant.js
+++ helper/constant.js
@@ -5,3 +5,18 @@ export const credential = {
     "password": "uCAYSxR7hlGK/63OzBFRQBWF60S7+n01nTbbxi822uHNKyscLAAiKAt40yNK/a0f",
     "id_pengguna": "272fbc02-cf64-4ad9-9363-33afd4d55198"
 }
+
+export function dynamicSort(property) {
+    var sortOrder = 1;
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a,b) {
+        /* next line works with strings and numbers, 
+         * and you may want to customize it to your needs
+         */
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+    }
+}
