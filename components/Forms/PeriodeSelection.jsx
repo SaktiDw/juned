9b25d2ded67fbdb8660be06da8245337dcef3848
SaktiDw <sapeiidw@gmail.com>
2023-04-21 15:35:57 +0800

    change default value periode selection

--- components/Forms/PeriodeSelection.jsx
+++ components/Forms/PeriodeSelection.jsx
@@ -2,7 +2,7 @@ import { fetchSemester } from "@/helper/api/apiSister";
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
-const PeriodeSelection = () => {
+const PeriodeSelection = ({ onChange }) => {
   const { data, isLoading, error } = useQuery({
     queryKey: ["periode"],
     queryFn: () => fetchSemester(),
@@ -14,7 +14,10 @@ const PeriodeSelection = () => {
         name="periode"
         id="periode"
         className="py-2 px-4 bg-white dark:bg-slate-700 rounded-md shadow-xl"
+        onChange={onChange}
+        defaultValue={""}
       >
+        <option value="">Semua periode</option>
         {data &&
           data.map((item, index) => (
             <option key={item.id} value={item.id}>
