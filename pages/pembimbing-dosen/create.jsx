31ecf4a943a2b8c3de60bfc865109f19d017dd6c
SaktiDw <sapeiidw@gmail.com>
2023-05-08 15:56:57 +0800

    bimbing dosen

--- pages/pembimbing-dosen/create.jsx
+++ pages/pembimbing-dosen/create.jsx
@@ -5,10 +5,10 @@ const BimbingDosenCreate = () => {
   return (
     <MainLayout>
       <div className="flex flex-col gap-8">
-        <Nav title={"Edit Lain"} />
+        <Nav title={"Tambah Bimbing Dosen"} />
         <div className="flex flex-col gap-4 dark:text-white w-full h-full">
           <h1 className="text-md font-bold uppercase">
-            Form Tambah Bahan Ajar
+            Form Tambah Bimbing Dosen
           </h1>
           <div className="grid grid-flow-col gap-4 bg-white dark:bg-slate-800 px-4 py-8 rounded-xl ">
             <FormCreateBimbingDosen />
